9427443781d414c53e2d045677bd6efbd67e1d79
TheChampion96 <Kadjmali@gmail.com>
2020-08-05 21:00:02 +0200

    desktop styling completed

---
+++ js.js
@@ -0,0 +1,15 @@
+// const share = document.querySelector('.share > img');
+// const close = document.querySelector('.close');
+// const social = document.querySelector('.social-media');
+// // console.log(share);
+
+// share.addEventListener('click', () => {
+
+//     if(true) {
+//         close.style.display = 'none';
+//     } else {
+//         close.style.display = 'flex';
+//     }
+    
+//     social.style.display = 'flex';
+// })
